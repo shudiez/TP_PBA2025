dce1ffd32bdd64ec2098964137c9fd6d7ed340e1
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-23 19:47:34 -0300

    Update Celda.java

--- Celda.java
+++ Celda.java
@@ -2,6 +2,11 @@ package tp1_pba;
 
 //Una celda por cada celda que tenga el mapa
 public class Celda {
+private Personaje personaje;
+    private Item item;
 
+    public void  setPersonaje(Personaje personaje){
+        this.personaje= personaje;
+    }
     
 }
