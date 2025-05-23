ec547ad0eabe3cb540b1950340f73cb85cbe8cf0
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-23 14:48:06 -0300

    Update Guardia.java

--- Guardia.java
+++ Guardia.java
@@ -1,19 +1,19 @@
 package tp1_pba;
 
+//Enemigo patrullante, hereda de Personaje e implementa Enemigo
 public class Guardia extends Personaje implements Enemigo {
 
     //Constructor
-    public Guardia(String nombre_personaje, int vida_personaje, boolean estaVivoONo, char inicial_personaje, Posicion posicion_personaje) {
-        super(nombre_personaje, vida_personaje, estaVivoONo, inicial_personaje, posicion_personaje);
+    public Guardia(String nombre_personaje, int vida_personaje, boolean estaVivo, char inicial_personaje, Posicion posicion_personaje) {
+        super(nombre_personaje, vida_personaje, estaVivo, inicial_personaje, posicion_personaje);
     }
 
-    
     //Metodos
     @Override
     public void mover() {
         //Falta cargar
     }
-
+    /*
     @Override
     public void patrullar() {
 //falta cargar
@@ -23,5 +23,5 @@ public class Guardia extends Personaje implements Enemigo {
     public void atacar() {
 //falta cargar
     }
-
+     */
 }
