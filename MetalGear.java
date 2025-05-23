124787c19c730eb1a3d8323fd51106de0c26b4ad
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-23 14:50:56 -0300

    Update MetalGear.java

--- MetalGear.java
+++ MetalGear.java
@@ -1,6 +1,12 @@
 package tp1_pba;
 
+//Enemigo final, hereda de Personaje e implementa Enemigo
 public class MetalGear extends Personaje implements Enemigo {
+
+    //Constructor
+    public MetalGear(String nombre_personaje, int vida_personaje, boolean estaVivo, char inicial_personaje, Posicion posicion_personaje) {
+        super(nombre_personaje, vida_personaje, estaVivo, inicial_personaje, posicion_personaje);
+    }
 
     //Metodos que implementa de la clase abstacta Enemigo
     @Override
@@ -8,14 +14,18 @@ public class MetalGear extends Personaje implements Enemigo {
 //falta cargar
     }
 
-    @Override
     public void patrullar() {
 //falta cargar
     }
 
-    @Override
     public void atacar() {
 //falta cargar
     }
+
+    boolean estaVivo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+  
 
 }
