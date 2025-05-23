70d4a36d5f8e3e19668d17c6519b16a76579f239
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-23 14:51:49 -0300

    Update Posicion.java

--- Posicion.java
+++ Posicion.java
@@ -1,44 +1,49 @@
 package tp1_pba;
 
+//clase auxiliar que contiene una posicion en X , Y
 public class Posicion {
 
     //atributos
-    private int jugadorEnPosicionN;
-    private int jugarorEnPosicionM;
+    private int posicionX;
+    private int posicionY;
 
     //Getters y setters
-    public int getJugadorEnPosicionN() {
-        return jugadorEnPosicionN;
+    public int getPosicionX() {
+        return posicionX;
     }
 
-    public void setJugadorEnPosicionN(int jugadorEnPosicionN) {
-        this.jugadorEnPosicionN = jugadorEnPosicionN;
+    public void setPosicionX(int posicionX) {
+        this.posicionX = posicionX;
     }
 
-    public int getJugarorEnPosicionM() {
-        return jugarorEnPosicionM;
+    public int getPosicionY() {
+        return posicionY;
     }
 
-    public void setJugarorEnPosicionM(int jugarorEnPosicionM) {
-        this.jugarorEnPosicionM = jugarorEnPosicionM;
+    public void setPosicionY(int posicionY) {
+        this.posicionY = posicionY;
     }
 
     //constructor 
-    public Posicion(int jugadorEnPosicionN, int jugarorEnPosicionM) {
-        this.jugadorEnPosicionN = jugadorEnPosicionN;
-        this.jugarorEnPosicionM = jugarorEnPosicionM;
+    public Posicion(int posicionX, int posicionY) {
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
     }
 //metodo avanzar()
-    public void avanzar(){
-        this.jugadorEnPosicionN++;
+
+    public void avanzar() {
+        this.posicionX++;
     }
 //Metodo retroceder()
-     public void retroceder(){
-        this.jugadorEnPosicionN--;
+
+    public void retroceder() {
+        this.posicionX--;
     }
+
+//Metodo toString()
     @Override
     public String toString() {
-        return "(" + jugadorEnPosicionN + "," + jugadorEnPosicionN + ").";
+        return "(" + posicionX + "," + posicionY + ").";
     }
-    
+
 }
