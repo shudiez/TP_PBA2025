bf026760c756d6d7654979b930693722a32b84ec
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-23 14:48:51 -0300

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -1,9 +1,73 @@
 package tp1_pba;
 
+//CLASE PRINCIPAL
+import java.util.Scanner;
+
+//datos generales y config del juego
 public class Juego {
 
-    //metodos
-    public void iniciar() {
+    //private Snake snake;
+    public static void main(String[] args) {
+        //metodos
+ 
+        public void iniciar() {
         //falta cargar
     }
-}
+        
+ public void iniciarMision() {
+        //falta cargar
+    }
+      
+
+    public void guardarEstado() {
+        //falta cargar
+    }
+
+    public void cargarEstado() {
+        //falta cargar
+    }
+
+    public void salirJuego() {
+        //falta cargar
+    }
+         
+        Scanner teclado = new Scanner(System.in); //inicializo el teclado para el usuario
+
+        Juego juego = new Juego(); // Doy inicio a un nuevo juego
+
+        char opcion;
+
+        do {
+            System.out.println("----------BIENVENIDO!----------");
+            System.out.println("----------MENU PRINCIPAL----------");
+            System.out.println("1- Iniciar mision");
+            System.out.println("2- Guardar estado");
+            System.out.println("3- Cargar estado");
+            System.out.println("4- Salir");
+
+            opcion = teclado.next().charAt(0); //lo uso para que tome la primera letra
+
+            switch (opcion) {
+                case 1:
+                    juego.iniciarMision();
+
+                    break;
+
+                case 2:
+                    juego.guardarEstado();
+
+                    break;
+
+                case 3:
+                    juego.cargarEstado();
+
+                    break;
+                case 4:
+                    juego.salirJuego();
+
+                default:
+                    System.out.println("La opción ingresada no es valida.");
+
+            }
+        }
+    }
