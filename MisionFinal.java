c613ef4146d795b77b97055a2d0a5069ebe89ab0
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-23 14:50:12 -0300

    Update MisionFinal.java

--- MisionFinal.java
+++ MisionFinal.java
@@ -2,10 +2,35 @@ package tp1_pba;
 
 public class MisionFinal extends Mision {
 
-    //metodos
-    @Override
-    public void iniciar() {
-//falta cargar
+    // Constructor
+    public MisionFinal() {
+        super("Hangar de Metal Gear");
     }
 
+    // Metodo para iniciar
+    /* @Override
+  public boolean iniciar(Snake snake) {
+        System.out.println("--- MISIÓN 3: BATALLA FINAL ---");
+        System.out.println("¡Metal Gear REX aparece!");
+    }
+     */
+    // Creo Personaje MetalGear de nombre rex
+    //  MetalGear rex = new MetalGear();
+
+    /* while (snake.estaVivo() && .estaVivo()) {
+            System.out.println("Tu vida: " + snake.getVida() + " HP | Vida de REX: " + rex.getVida() + " HP");
+            System.out.println("Turno de Snake:");
+            System.out.println("1 - Disparar misil");
+            System.out.println("2 - Esquivar");
+
+            // ----------------FALTA COMPLETAR----------------
+        }
+          
+   
+     */
+
+    @Override
+    public boolean iniciar(Snake snake) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
