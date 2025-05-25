3d42b7c9d166142e4e92449eed7a80c43996792f
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 19:58:09 -0300

    Update MisionFinal.java

--- MisionFinal.java
+++ MisionFinal.java
@@ -1,36 +1,55 @@
 package tp1_pba;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class MisionFinal extends Mision {
 
-    // Constructor
-    public MisionFinal() {
-        super("Hangar de Metal Gear");
+    //Constructor
+    public MisionFinal(boolean completa) {
+        super(completa);
     }
-
-    // Metodo para iniciar
-    /* @Override
-  public boolean iniciar(Snake snake) {
-        System.out.println("--- MISIÓN 3: BATALLA FINAL ---");
-        System.out.println("¡Metal Gear REX aparece!");
-    }
-     */
-    // Creo Personaje MetalGear de nombre rex
-    //  MetalGear rex = new MetalGear();
-
-    /* while (snake.estaVivo() && .estaVivo()) {
-            System.out.println("Tu vida: " + snake.getVida() + " HP | Vida de REX: " + rex.getVida() + " HP");
-            System.out.println("Turno de Snake:");
-            System.out.println("1 - Disparar misil");
-            System.out.println("2 - Esquivar");
-
-            // ----------------FALTA COMPLETAR----------------
-        }
-          
-   
-     */
 
     @Override
-    public boolean iniciar(Snake snake) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void iniciar(Snake snake, Mapa mapa) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("¡Metal Gear REX aparece!");
+        while (snake.estaVivo() && rex.estaVivo()) {
+            System.out.println("Tu vida: " + snake.getVida() + "HP" + " | Vida de REX: " + rex.getVida() + "HP");
+            System.out.println("1 - Disparar misil");
+            System.out.println("2 - Esquivar");
+            String opcion = scanner.nextLine();
+
+            if (opcion.equals("1")) {
+
+                Random random = new Random();
+
+                //esto va a hacer un daño aleatorio entre 10 y 30 a Rex
+                int danio = random.nextInt(30 - 10 + 1) + 10;
+                rex.recibirDanio(danio);
+                System.out.println("¡Le diste a REX! -" + danio + " HP");
+                System.out.println("Vida de REX: " + rex.getVida() + "HP");
+            } else if (opcion.equals("2")) {
+                int danio = rex.atacar() / 2;
+                snake.recibirDanio(danio);
+                System.out.println("¡Esquivaste! Daño reducido a " + danio);
+            }
+
+            if (rex.estaVivo()) {
+                int ataque = rex.atacar();
+                snake.recibirDanio(ataque);
+                System.out.println("Metal Gear ataca con un Cañón Láser! -" + ataque + " HP");
+            }
+        }
+
+        if (snake.estaVivo()) {
+            System.out.println("¡Ganaste! REX fue destruido.");
+            completa = true;
+        } else {
+            System.out.println("Game over. Snake ha sido derrotado.");
+        }
     }
 }
+
