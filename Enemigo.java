3bf13e1efd7ffdacbce810def9c64243e0a7b086
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 18:45:29 -0300

    Update Enemigo.java

--- Enemigo.java
+++ Enemigo.java
@@ -3,8 +3,9 @@ package tp1_pba;
 //Interfaz para comportamiento de enemigos
 public interface Enemigo {
 
-  //meotodos
-  public void patrullar();
-  public void atacar();
+    //meotodos
+    public void patrullar(Mapa mapa);
+
+    public boolean detectarSnake(Snake snake);
 
 }
