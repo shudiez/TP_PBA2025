d58aa38581d3ca3923803126d253acdd05439fef
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 18:54:33 -0300

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -3,29 +3,46 @@ package tp1_pba;
 //Jugador, hereda de Personaje
 public class Snake extends Personaje {
 
-    //Atributos 
-    
-
     //constructor
-    public Snake (Posicion posicion){
-super("Snake",posicion)
+    public Snake(String nombre, int vida, char inicial, Posicion posicion) {
+        super("Snake", vida, 'S', posicion);
     }
-    
 
-    //metodo
-
-
+    //metodos
     @Override
-    public void mover(string direccion) {
-        switch(direccion){
-               case: "arriba" -> posicion.posicionY--;
-                case: "abajo" -> posicion.posicionY++;
-                case: "izquierda" -> posicion. posicionX--;
-                case: "derecha" -> posicion.posicionX++;
-                  
+    public void mover(String direccion, Mapa mapa) {
+        int x = posicion.getX();
+        int y = posicion.getY();
+
+        if (direccion.equals("Arriba") && x > 0) {
+            posicion.setX(x - 1);
+        } else if (direccion.equals("Abajo") && x < 6) {
+            posicion.setX(x + 1);
+        } else if (direccion.equals("Izquierda") && y > 0) {
+            posicion.setY(y - 1);
+        } else if (direccion.equals("Derecha") && y < 6) {
+            posicion.setY(y + 1);
+        } else {
+            System.out.println("La direccion no es correcta.");
         }
-        system.out.println{"Snake se movio a la " + direccion};
-        
     }
-    
 }
+
+/* public void dispararMisil(Personaje Metalgear) {
+        //Si le dispara a Rex(Metal Gear), le resta -20 HP
+        Random random = new Random();
+
+        //esto va a hacer un daño aleatorio entre 10 y 30 a Rex
+        int danio = random.nextInt(30 - 10 + 1) + 10;
+
+        //MetalGear.recibirDanio(danio);
+        //System.out.println("¡Le diste a " + MetalGear.getNombre() + ("! ) ((-") + danio + (" HP") );
+        //System.out.println("Vida de " + MetalGear.getNombre() + (":") + MetalGear.vida + (" HP"));
+    }
+
+    public void esquivar() {
+
+    }
+
+}
+ */
