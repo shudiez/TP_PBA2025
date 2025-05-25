5699c4725fc21a112752eeccee2483181ea10289
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 19:03:02 -0300

    Update Posicion.java

--- Posicion.java
+++ Posicion.java
@@ -13,21 +13,33 @@ public class Posicion {
         this.y = y;
     }
 
-    //Getters y setters
+    //Getters 
     public int getX() {
         return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
     }
 
     public int getY() {
         return y;
     }
 
+    //Setters
+    public void setX(int x) {
+        this.x = x;
+    }
+
     public void setY(int y) {
         this.y = y;
+    }
+
+    public boolean esIgual(Posicion otraPosicion) {
+        return this.x == otraPosicion.x && this.y == otraPosicion.y;
+
+    }
+
+    public int distancia(Posicion otraPosicion) {
+        int dx = Math.abs(this.x - otraPosicion.x);
+        int dy = Math.abs(this.y - otraPosicion.y);
+        return dx + dy;
     }
 
 }
