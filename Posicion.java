600e8d9baf2d6f73c8c5e6f1162e13f0cb92ff8b
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 18:00:16 -0300

    Update Posicion.java

--- Posicion.java
+++ Posicion.java
@@ -4,46 +4,30 @@ package tp1_pba;
 public class Posicion {
 
     //atributos
-    private int posicionX;
-    private int posicionY;
-
-    //Getters y setters
-    public int getPosicionX() {
-        return posicionX;
-    }
-
-    public void setPosicionX(int posicionX) {
-        this.posicionX = posicionX;
-    }
-
-    public int getPosicionY() {
-        return posicionY;
-    }
-
-    public void setPosicionY(int posicionY) {
-        this.posicionY = posicionY;
-    }
+    private int x;
+    private int y;
 
     //constructor 
-    public Posicion(int posicionX, int posicionY) {
-        this.posicionX = posicionX;
-        this.posicionY = posicionY;
-    }
-//metodo avanzar()
-
-    public void avanzar() {
-        this.posicionX++;
-    }
-//Metodo retroceder()
-
-    public void retroceder() {
-        this.posicionX--;
+    public Posicion(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-//Metodo toString()
-    @Override
-    public String toString() {
-        return "(" + posicionX + "," + posicionY + ").";
+    //Getters y setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
 }
