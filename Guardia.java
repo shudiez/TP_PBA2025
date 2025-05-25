dab0430a3441f80482920c8036a7d0982cca2a21
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 19:03:57 -0300

    Update Guardia.java

--- Guardia.java
+++ Guardia.java
@@ -4,24 +4,31 @@ package tp1_pba;
 public class Guardia extends Personaje implements Enemigo {
 
     //Constructor
-    public Guardia(Posicion posicion){
-        super("Guardia",posicion);
+    public Guardia(String nombre, int vida, char inicial, Posicion posicion) {
+        super("Guardia", vida, '*', posicion);
     }
 
     //Metodos
     @Override
-    public void mover(string direccion) {
-        //Falta cargar
-    }
-    
-    @Override
-    public void patrullar() {
-//falta cargar
+    public void mover(String direccion, Mapa mapa) {
+
     }
 
     @Override
-    public void atacar() {
-//falta cargar
+    public void patrullar(Mapa mapa) {
+        int x = posicion.getX();
+        int y = posicion.getY();
+
+        if (x + 1 < 7) {
+            posicion.setX(x + 1);
+        } else {
+            posicion.setX(x - 1);
+        }
     }
-     
+
+    @Override
+    public boolean detectarSnake(Snake snake) {
+        return posicion.distancia(snake.getPosicion()) <= 1;
+    }
+
 }
