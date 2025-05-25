65afcc000a32dc4d00ca3491b681f9b50791d770
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-25 18:21:50 -0300

    Update Celda.java

--- Celda.java
+++ Celda.java
@@ -2,11 +2,36 @@ package tp1_pba;
 
 //Una celda por cada celda que tenga el mapa
 public class Celda {
-private Personaje personaje;
-    private Item item;
 
-    public void  setPersonaje(Personaje personaje){
-        this.personaje= personaje;
+    private char inicial; //Para S,*,G,C,P,L
+    private boolean cerrada;
+
+    //CONSTRUCTOR
+    public Celda(char inicial, boolean cerrada) {
+        this.inicial = inicial;
+        this.cerrada = false;
     }
-    
+
+    //Getters
+    public char getInicial() {
+        return inicial;
+    }
+
+    //Setters
+    public void setInicial(char inicial) {
+        this.inicial = inicial;
+    }
+
+    public boolean isCerrada() {
+        return cerrada;
+    }
+
+    //Metodos
+    public void cerrar() {
+        this.cerrada = true;
+    }
+
+    public void abrir() {
+        this.cerrada = false;
+    }
 }
