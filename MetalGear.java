9dac26583c48e9195a1a2223fea59516b773173c
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-27 13:38:57 -0300

    Update MetalGear.java

--- MetalGear.java
+++ MetalGear.java
@@ -1,40 +1,11 @@
-import java.util.Random;
 
-public class MetalGear extends Personaje implements Enemigo {
-   
-    //CONSTRUCTOR
-    public MetalGear(Posicion posicion) {
-        super("Metal Gear REX", 100, 'M', posicion);
-    }
+public abstract class Mision {
 
-    //METODOS
-    @Override
-    public void patrullar(Mapa mapa) {
-        // No patrulla, es fijo
-    }
+    protected boolean completada = false;
 
-    @Override
-    public boolean detectarSnake(Snake snake) {
-        // No se usa en la batalla final
-        return false;
-    }
+    public abstract void iniciar(Snake snake, Mapa mapa);
 
-    @Override
-    public void atacar(Snake snake) {
-        Random rand = new Random();
-        int danio = rand.nextInt(26) + 15; // 15 a 40
-        snake.recibirDanio(danio);
-    }
-
-    public void recibirDanio(int danio) {
-        this.vida = this.vida - danio;
-        if (this.vida < 0) {
-            this.vida = 0;
-        }
-    }
-
-    @Override
-    public void mover(Mapa mapa, String direccion) {
-        //No se mueve, es fijo
+    public boolean estaCompleta() {
+        return completada;
     }
 }
