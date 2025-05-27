eb60aa4ffa68a734162ebab3ae43f4236a951f53
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-27 13:39:23 -0300

    Update Mision.java

--- Mision.java
+++ Mision.java
@@ -1,21 +1,11 @@
-package tp1_pba;
 
-//Gestiona los objetivos y desbloqueo de zonas
 public abstract class Mision {
 
-//Atributos
-    protected boolean completa;
+    protected boolean completada = false;
 
-//Constructor
-    public Mision(boolean completa) {
-        this.completa = false;
-    }
-
-    //Getter
-    public boolean isCompleta() {
-        return completa;
-    }
-
-//Metodos
     public abstract void iniciar(Snake snake, Mapa mapa);
+
+    public boolean estaCompleta() {
+        return completada;
+    }
 }
