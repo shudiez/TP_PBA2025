937e0b76bbe251aac1045cbb84f1e8900f4ae18e
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-27 13:40:28 -0300

    Update Posicion.java

--- Posicion.java
+++ Posicion.java
@@ -1,19 +1,14 @@
-package tp1_pba;
-
-//clase auxiliar que contiene una posicion en X , Y
 public class Posicion {
-
-    //atributos
     private int x;
     private int y;
 
-    //constructor 
+    //CONSTRUCTOR
     public Posicion(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    //Getters 
+    //GETTERS AND SETTERS
     public int getX() {
         return x;
     }
@@ -22,7 +17,6 @@ public class Posicion {
         return y;
     }
 
-    //Setters
     public void setX(int x) {
         this.x = x;
     }
@@ -30,16 +24,4 @@ public class Posicion {
     public void setY(int y) {
         this.y = y;
     }
-
-    public boolean esIgual(Posicion otraPosicion) {
-        return this.x == otraPosicion.x && this.y == otraPosicion.y;
-
-    }
-
-    public int distancia(Posicion otraPosicion) {
-        int dx = Math.abs(this.x - otraPosicion.x);
-        int dy = Math.abs(this.y - otraPosicion.y);
-        return dx + dy;
-    }
-
 }
