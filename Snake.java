fda4388a5cfd5252cf31dc20783ee53c292c00ed
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-27 13:40:42 -0300

    Update Snake.java

--- Snake.java
+++ Snake.java
@@ -1,48 +1,21 @@
-package tp1_pba;
-
-//Jugador, hereda de Personaje
 public class Snake extends Personaje {
-
-    //constructor
-    public Snake(String nombre, int vida, char inicial, Posicion posicion) {
-        super("Snake", vida, 'S', posicion);
+    
+    //CONSTRUCTOR
+    public Snake(Posicion posicion) {
+        super("Snake", 100, 'S', posicion);
     }
 
-    //metodos
+    //METODOS
     @Override
-    public void mover(String direccion, Mapa mapa) {
-        int x = posicion.getX();
-        int y = posicion.getY();
+    public void mover(Mapa mapa, String direccion) {
+        // Movimiento del jugador, ej: "arriba", "abajo", "izq", "der"
+        mapa.moverPersonaje(this, direccion);
+    }
 
-        if (direccion.equals("Arriba") && x > 0) {
-            posicion.setX(x - 1);
-        } else if (direccion.equals("Abajo") && x < 6) {
-            posicion.setX(x + 1);
-        } else if (direccion.equals("Izquierda") && y > 0) {
-            posicion.setY(y - 1);
-        } else if (direccion.equals("Derecha") && y < 6) {
-            posicion.setY(y + 1);
-        } else {
-            System.out.println("La direccion no es correcta.");
+    public void recibirDanio(int danio) {
+        this.vida = this.vida - danio;
+        if (this.vida < 0) {
+            this.vida = 0;
         }
     }
 }
-
-/* public void dispararMisil(Personaje Metalgear) {
-        //Si le dispara a Rex(Metal Gear), le resta -20 HP
-        Random random = new Random();
-
-        //esto va a hacer un daño aleatorio entre 10 y 30 a Rex
-        int danio = random.nextInt(30 - 10 + 1) + 10;
-
-        //MetalGear.recibirDanio(danio);
-        //System.out.println("¡Le diste a " + MetalGear.getNombre() + ("! ) ((-") + danio + (" HP") );
-        //System.out.println("Vida de " + MetalGear.getNombre() + (":") + MetalGear.vida + (" HP"));
-    }
-
-    public void esquivar() {
-
-    }
-
-}
- */
