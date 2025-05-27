067b4455462b9b32183c5a96660b943311926f5d
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-27 13:38:21 -0300

    Update Mapa.java

--- Mapa.java
+++ Mapa.java
@@ -1,57 +1,87 @@
-package tp1_pba;
 
 public class Mapa {
 
-    //Atributos
-    private int N; //Filas
-    private int M; //Columnas
-
-    private Celda[][] celdas;     //creo matriz para recorrer
+    private Celda[][] celdas;
+    private int filasN;
+    private int columnasM;
 
     //CONSTRUCTOR
-    public Mapa(int N, int M) {
-        this.N = N;
-        this.M = M;
-        celdas = new Celda[N][M];
-        iniciarCeldas();
+    public Mapa(int filasN, int columnasM) {
+        this.filasN = filasN;
+        this.columnasM = columnasM;
+        celdas = new Celda[filasN][columnasM];
+        for (int i = 0; i < filasN; i++) {
+            for (int j = 0; j < columnasM; j++) {
+                celdas[i][j] = new Celda(' ');
+            }
+        }
     }
 
-    //metodos
-    public void colocarPersonaje(Personaje personaje, Posicion position) {
-
+    //GETTERS
+    public int getfilasN() {
+        return filasN;
     }
 
-    public void moverPersonaje() {
-        //falta cargar
+    public int getcolumnasM() {
+        return columnasM;
+    }
 
+    public Celda getCelda(int x, int y) {
+        return celdas[x][y];
+    }
+
+    //METODOS
+    public void colocarPersonaje(Personaje p, Posicion pos) {
+        Celda celda = celdas[pos.getX()][pos.getY()];
+        celda.setPersonaje(p);
+        celda.setContenido(p.getinicial()); // SIEMPRE poner el símbolo del personaje
+        p.setPosicion(pos);
+    }
+
+    public void moverPersonaje(Personaje p, String direccion) {
+        // 1. Sacar personaje de la celda actual y restaurar contenido si hay Objeto
+        Posicion actual = p.getPosicion();
+        Celda celdaActual = celdas[actual.getX()][actual.getY()];
+        celdaActual.setPersonaje(null);
+
+        if (celdaActual.getObjeto() != null) {
+            celdaActual.setContenido(celdaActual.getObjeto().getinicial());
+        } else {
+            celdaActual.setContenido(' '); // Si ya no hay ítem, dejar vacío
+        }
+
+        // 2. Calcular nueva posición
+        int x = actual.getX();
+        int y = actual.getY();
+        if (direccion.equals("arriba") && x > 0) {
+            x--;
+        } else if (direccion.equals("abajo") && x < filasN - 1) {
+            x++;
+        } else if (direccion.equals("izq") && y > 0) {
+            y--;
+        } else if (direccion.equals("der") && y < columnasM - 1) {
+            y++;
+        }
+
+        // 3. Mover personaje a la nueva celda
+        Posicion nueva = new Posicion(x, y);
+        colocarPersonaje(p, nueva);
     }
 
     public void mostrar() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                System.out.print(celdas[i][j].getInicial() + " ");
+        System.out.println("----- MAPA -----");
+        for (int i = 0; i < filasN; i++) {
+            for (int j = 0; j < columnasM; j++) {
+                char contenido = celdas[i][j].getContenido();
+                // Si la celda está vacía, mostrá un punto para que se vea la grilla
+                if (contenido == ' ') {
+                    System.out.print(". ");
+                } else {
+                    System.out.print(contenido + " ");
+                }
             }
+            System.out.println(); // salto de línea al final de cada fila
         }
+        System.out.println("---------------");
     }
-
-    private void iniciarCeldas() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                celdas[i][j] = new Celda(" ");
-            }
-        };
-    }
-
-    public void ponerInicial(int x, int y, char inicial) {
-        if (x >= 0 && x < N && y >= 0 && y < M) {
-            celdas[x][y].setInicial(inicial);
-        }
-    }
-
-    /*   public char obtenerInicial(int x, int y) {
-        if (x >= 0 && x < N && y >= 0 && y < M) {
-            return celdas[x][y].getInicial();
-        }
-    }
-     */
 }
