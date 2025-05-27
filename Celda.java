35d9725b15c932faacadf2b26ea48d5c020a2f57
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-27 13:37:05 -0300

    Update Celda.java

--- Celda.java
+++ Celda.java
@@ -1,37 +1,43 @@
-package tp1_pba;
-
-//Una celda por cada celda que tenga el mapa
 public class Celda {
-
-    private char inicial; //Para S,*,G,C,P,L
-    private boolean cerrada;
+    private char contenido; // Ej: ' ', 'S', '*', 'L', 'C', etc.
+    private Personaje personaje;
+    private Objeto Objeto;
 
     //CONSTRUCTOR
-    public Celda(char inicial, boolean cerrada) {
-        this.inicial = inicial;
-        this.cerrada = false;
+    public Celda(char contenido) {
+        this.contenido = contenido;
+        this.personaje = null;
+        this.Objeto = null;
     }
 
-    //Getters
-    public char getInicial() {
-        return inicial;
+    //GETTERS AND SETTERS
+    public char getContenido() {
+        return contenido;
     }
 
-    //Setters
-    public void setInicial(char inicial) {
-        this.inicial = inicial;
+    public void setContenido(char contenido) {
+        this.contenido = contenido;
     }
 
-    public boolean isCerrada() {
-        return cerrada;
+    public Personaje getPersonaje() {
+        return personaje;
     }
 
-    //Metodos
-    public void cerrar() {
-        this.cerrada = true;
+    public void setPersonaje(Personaje personaje) {
+        this.personaje = personaje;
     }
 
-    public void abrir() {
-        this.cerrada = false;
+    public Objeto getObjeto() {
+        return Objeto;
+    }
+
+    public void setObjeto(Objeto Objeto) {
+        this.Objeto = Objeto;
+    }
+
+    //METODOS
+    public boolean estaVacia() {
+        return contenido == ' ' && personaje == null && Objeto == null;
     }
 }
+
