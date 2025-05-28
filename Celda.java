3c032dad72f4e9425fd560a4506ca10e6d24bcd4
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-28 14:52:00 -0300

    Update Celda.java

--- Celda.java
+++ Celda.java
@@ -1,17 +1,29 @@
-
+/*
+Esta clase representa a una SOLA celda del mapa del juego. 
+*/
 public class Celda {
 
-    private char inicial; // Ej: ' ', 'S', '*', 'L', 'C', etc.
+    //Una inicial (como ' ', 'S', '*', 'L', 'C'.) que es lo que se va a ver en el mapa
+    private char inicial; 
+    //Un personaje que es el que va a estar parado en la celda (Snake, guardia).
     private Personaje personaje;
-    private Objeto Objeto;
+    //Un objeto, que nos va a servir para agarrarlo como la llave, C4 o por ejemplo entrar al Hangar, la puerta, etc.
+    private Objeto objeto;
 
     //CONSTRUCTOR
+    /*
+    Cuando creo una celda, le tengo que pasar la inicial que quiero que tenga. Esto puede cambiar despues con los setters igual.
+    */
     public Celda(char inicial) {
         this.inicial = inicial;
 
     }
 
     //GETTERS AND SETTERS
+    /*
+    Los metodos get y set van a  leer o modificar los atributos privados. 
+    Uso esto para mantener el encapsulamiento.
+    */
     public char getInicial() {
         return inicial;
     }
@@ -29,13 +41,13 @@ public class Celda {
     }
 
     public Objeto getObjeto() {
-        return Objeto;
+        return objeto;
     }
 
     public void setObjeto(Objeto Objeto) {
-        this.Objeto = Objeto;
+        this.objeto = Objeto;
     }
 
-    //METODOS
-  
+
+   
 }
