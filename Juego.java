2aded48863bb53d793f63026fefff3a14dbbd1d1
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-28 14:43:56 -0300

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -8,12 +8,12 @@ public class Juego {
     public void mostrarMenu() {
 
         Scanner scanner = new Scanner(System.in);
-
-        boolean salir = false;
-        //Arme un menu dentro de un while que se repite hasta que el jugador ponga la opcion 4 "salir"
+   //Arme un menu dentro de un while que se repite hasta que el jugador ponga la opcion 4 "salir"
         // El menú tiene 4 opciones: 1. Inicia la mision. 2. Muestra  el nunero de misiones conpletadas. 
         //3. Permite que el jugador cargue su progreso anterior nediante un codigo. esto se guarda en misionesConpletas. 
         //4. Sale del juego
+        boolean salir = false;
+    
         while (!salir) {
             System.out.println("----- MENU PRINCIPAL -----");
             System.out.println("1. Iniciar mision");
@@ -49,7 +49,12 @@ public class Juego {
         }
 
     }
-
+//Segun el num de misiones completas,, se va desbloqueando la que le sigue:
+//Si misionesCompletas == 0, se crea el mapa de 7x7 y empieza la Mision 1.
+//Si misionesCompletas == 1, se crea el mapa de 9x9 y empieza la Mision 2.
+//Si misionesCompletas == 2, empieza la Mision Final.
+//Si se completo todo, se imprime “¡El juego ha terminado!”.
+//Cada vez que una mision se completa (usando mision.estaCompleta()), se suma uno a misionesCompletas.
     private void iniciarMision() {
         if (misionesCompletas == 0) {
             // Mision 1
@@ -82,7 +87,10 @@ public class Juego {
             System.out.println("El juego ha terminado!");
         }
     }
-
+/*
+   El main va a ser donde le damos play al programa. 
+    Crea un objeto de la clase Juego y llama al metodo mostrarMenu() para arrancar el juego.
+    */
     public static void main(String[] args) {
         Juego juego = new Juego();
         juego.mostrarMenu();
