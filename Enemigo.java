1bca2f9773d7f1644ed436060745035a73cc6098
Yesica  V. Diez <69434927+shudiez@users.noreply.github.com>
2025-05-28 14:55:43 -0300

    Update Enemigo.java

--- Enemigo.java
+++ Enemigo.java
@@ -1,9 +1,12 @@
 
 public interface Enemigo {
 
+    // Este metodo va a hacer que los enemigos tengan un comportamiento de patrullaje, o sea, que se muevan por el mapa de forma automatica.
     void patrullar(Mapa mapa);
 
+    //Este metodo devuelve true si el enemigo logra detectar a Snake a una celda de distancia, o false si no lo ve.
     boolean detectarSnake(Snake snake);
 
+    //Si el enemigo detecta a Snake, este metodo le permite atacarlo.
     void atacar(Snake snake);
 }
